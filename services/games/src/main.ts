d24bc8501566001b216fc6f87c020714fbb9f5af
Ging123 <iceglauber@outlook.com>
2026-04-18 18:31:29 -0300

    feat: add Keycloak configuration to .env.example, update package.json to include @nestjs/swagger, and enhance main.ts with Swagger setup for API documentation

--- services/games/src/main.ts
+++ services/games/src/main.ts
@@ -1,10 +1,25 @@
 import "reflect-metadata";
+import "dotenv/config";
+
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT;
+  const port = Number(process.env.PORT) || 4001;
+
+  const config = new DocumentBuilder()
+    .setTitle("Games API")
+    .setDescription("API documentation")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
+
   await app.listen(port, "0.0.0.0");
   console.log(`Games service running on port ${port}`);
 }
