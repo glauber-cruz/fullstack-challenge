901b5d6aedc5f681ae1bd096bda8fc2a5539bcb8
Ging123 <iceglauber@outlook.com>
2026-04-18 18:59:43 -0300

    feat: integrate DatabaseModule and RepositoryModule into AppModule for improved database and repository management

--- services/games/src/app.module.ts
+++ services/games/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from "@nestjs/common";
+import { DatabaseModule } from "./infrastructure/databases/database.module";
+import { RepositoryModule } from "./infrastructure/repositories/repository.module";
 import { GamesController } from "./presentation/controllers/games.controller";
 
 @Module({
+  imports: [DatabaseModule, RepositoryModule],
   controllers: [GamesController],
 })
 export class AppModule {}
