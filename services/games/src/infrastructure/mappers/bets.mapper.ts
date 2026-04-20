a9eaf93199d9deb13c664749a2ce99d55375d5f6
Ging123 <iceglauber@outlook.com>
2026-04-19 22:59:30 -0300

    feat: update BetsMapper to include status and processingStatus in entity and Prisma update methods

--- services/games/src/infrastructure/mappers/bets.mapper.ts
+++ services/games/src/infrastructure/mappers/bets.mapper.ts
@@ -2,6 +2,7 @@ import { Bet } from "@/domain/entites/bets.entity";
 
 import type { Bets } from "../../../generated/prisma/client";
 import { Amount } from "@/domain/value-object/amount";
+import { BetProcessingStatus, BetStatus } from "@/domain/enums/bet";
 
 export class BetsMapper {
   static toEntity(data: Bets): Bet {
@@ -12,6 +13,8 @@ export class BetsMapper {
       amount: Amount.create(Number(data.amount)),
       cashoutMultiplier: data.cashoutMultiplier ?? undefined,
       cashedOutAt: data.cashedOutAt ?? undefined,
+      status: data.status as BetStatus,
+      processingStatus: data.processingStatus as BetProcessingStatus,
       createdAt: data.createdAt,
       updatedAt: data.updatedAt,
     });
@@ -23,15 +26,18 @@ export class BetsMapper {
       userId: data.userId,
       roundId: data.roundId,
       amount: data.amount.cents,
+      status: data.status,
+      processingStatus: data.processingStatus,
       cashoutMultiplier: data.cashoutMultiplier,
       cashedOutAt: data.cashedOutAt,
-      
     };
   }
 
   static toPrismaUpdate(data: Bet) {
     return {
       amount: data.amount.cents,
+      status: data.status,
+      processingStatus: data.processingStatus,
       cashoutMultiplier: data.cashoutMultiplier,
       cashedOutAt: data.cashedOutAt,
     };
